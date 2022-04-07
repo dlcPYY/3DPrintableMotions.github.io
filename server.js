922f9b741968815a1bb538c9190925b5e077c2e4
dlcPYY <yux199911@gmail.com>
2022-04-07 11:09:37 +0800

    correct

---
+++ server.js
@@ -0,0 +1,12 @@
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
+let querystring = require('querystring');//操作参数模块
+
+function onRequest(request, response) {
+    let urlStr = url.parse(request.url);
+    let param = querystring.parse(urlStr.query);
+    console.log("收到请求.");
+    console.log(param);  
+}
+http.createServer(onRequest).listen(8888);
