c56f1b3e014f2dd759501359cfdc50672411bf6a
dlcPYY <yux199911@gmail.com>
2022-04-08 09:53:02 +0800

    ()

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let querystring = require('querystring');//操作参数模块
 function onRequest(request, response) {
     let urlStr = url.parse(request.url);
     let param = querystring.parse(urlStr.query);
-    console.log("收到请求.");
+    // console.log("收到请求.");
     console.log(param);  
 }
 http.createServer(onRequest).listen(8888);
